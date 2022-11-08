703ab52056462f7825df7890714a8b67fb7fbe4c
Rico Klimpel <rklimpel@slashwhy.de>
2022-11-08 16:39:58 +0100

    toggle like a radio box

--- Web/HTML-CSS-JS/change-parent-class-from-js/script.js
+++ Web/HTML-CSS-JS/change-parent-class-from-js/script.js
@@ -1,5 +1,9 @@
-function doStuff(){
-    const element = document.getElementById("btnTest");
-    let x = element.parentElement.classList.toggle("pink-background");
-    console.log(x);
+function toggleActive(el) {
+    let x = el.parentElement.classList.toggle("pink-background");
+    let otherElements = document.querySelectorAll('.btnBlubb');
+    otherElements.forEach(element => {
+        if (element !== el) {
+            element.parentElement.classList.remove("pink-background");
+        }
+    });
 }
